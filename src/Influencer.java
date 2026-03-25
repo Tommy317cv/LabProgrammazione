8fb36b5a9f29874e59f9295815a816ed88679d12
Tommaso <tommaso.lico@gmail.com>
2026-03-25 15:51:53 +0100

    Creazione delle classi e sottoclassi Utenti

---
+++ src/Influencer.java
@@ -0,0 +1,9 @@
+public class Influencer extends Utente {
+
+
+    private boolean SpuntaBlu;
+
+    public Influencer(String nickname, String password, String email) {
+        super(nickname, password, email);
+    }
+}
---
+++ src/Studente.java
@@ -0,0 +1,7 @@
+public class Studente extends  Utente{
+
+    private String CorsoLaurea;
+
+
+
+}
