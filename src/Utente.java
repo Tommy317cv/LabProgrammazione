c0b44a58a8607897d56b835c0a28ee9817ce258c
Tommaso <tommaso.lico@gmail.com>
2026-03-25 15:35:56 +0100

    Creazione della classe utente

---
+++ src/Utente.java
@@ -0,0 +1,13 @@
+public class Utente {
+    //Variabili di classe
+    private final String nickname;
+    private String password;
+    private String email;
+
+
+    public Utente(String nickname, String password, String email) {
+        this.nickname = nickname;
+        this.password = password;
+        this.email = email;
+    }
+}
