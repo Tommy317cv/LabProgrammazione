05ce804bef1120d4da6a3a8cc043ea3cdbb1c7de
Tommaso <tommaso.lico@gmail.com>
2026-03-27 13:13:55 +0100

    Creazione completa file e classi con correzione errori

--- src/Utente/Docente.java
+++ src/Utente/Docente.java
@@ -2,5 +2,7 @@ package Utente;
 
 public class Docente extends Utente {
 
-
+    public Docente(String nickname, String password, String email) {
+        super(nickname, password, email);
+    }
 }
--- src/Utente/Ricercatore.java
+++ src/Utente/Ricercatore.java
@@ -4,4 +4,10 @@ public class Ricercatore extends Utente {
 
     private int hIndex;
     private String ParolaChiave;
+
+    public Ricercatore(String nickname, String password, String email, int hIndex, String parolaChiave) {
+        super(nickname, password, email);
+        this.hIndex = hIndex;
+        ParolaChiave = parolaChiave;
+    }
 }
