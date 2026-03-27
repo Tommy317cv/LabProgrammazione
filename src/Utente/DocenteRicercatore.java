2b6ae5ff1c3698b0868324404aceb82bcb865067
Tommaso <tommaso.lico@gmail.com>
2026-03-27 13:15:22 +0100

    Creazione completa file e classi con correzione errori

--- src/Utente/DocenteRicercatore.java
+++ src/Utente/DocenteRicercatore.java
@@ -1,4 +1,8 @@
 package Utente;
 
 public class DocenteRicercatore extends Ricercatore {
+
+    public DocenteRicercatore(String nickname, String password, String email, int hIndex, String parolaChiave) {
+        super(nickname, password, email, hIndex, parolaChiave);
+    }
 }
