90bb8d8f29b28e8b7d8116a520a954a2836c7ea5
Tommaso <tommaso.lico@gmail.com>
2026-04-01 15:59:23 +0200

    Collegamenti e relazioni Utenti-Post

--- src/Utente/Utente.java
+++ src/Utente/Utente.java
@@ -1,10 +1,12 @@
 package Utente;
+import Post.Post;
 
 public class Utente {
     //Variabili di classe
     private final String nickname;
     private String password;
     private String email;
+    private Post[] PostDel_Utente;
 
 
     public Utente(String nickname, String password, String email) {
