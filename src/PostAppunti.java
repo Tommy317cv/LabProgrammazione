22fbe08cfc071cd054f696fdf3cf88e29580a1f8
vanessa <1albinivanessa@gmail.com>
2026-04-01 14:06:00 +0200

    Fusione

--- src/PostAppunti.java
+++
@@ -1,7 +0,0 @@
-import java.time.LocalDate;
-
-public class PostAppunti extends PostDocumentale{
-    public PostAppunti(LocalDate datapubblicazione, String testodescrittivo, String idalfanumerico, int dimensionetesto, int dimensionetitolo, int dimensionecitazione) {
-        super(datapubblicazione, testodescrittivo, idalfanumerico, dimensionetesto, dimensionetitolo, dimensionecitazione);
-    }
-}
--- src/PostTestuale.java
+++
@@ -1,7 +0,0 @@
-import java.time.LocalDate;
-
-public class PostTestuale extends Post{
-    public PostTestuale(LocalDate datapubblicazione, String testodescrittivo) {
-        super(datapubblicazione, testodescrittivo);
-    }
-}
